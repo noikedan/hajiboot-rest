d9df0f769e7e630098d5affae509b74097b9409a
Nobue Ikeda <no-ikeda@kvf.biglobe.ne.jp>
2021-01-12 18:46:10 +0900

    はじめてコミット

---
+++ src/main/java/com/example/util/YamlFooProperties.java
@@ -0,0 +1,35 @@
+package com.example.util;
+
+import java.util.List;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ConfigurationProperties(prefix = "yaml")
+@PropertySource(value = "classpath:foo.yml", factory = YamlPropertySourceFactory.class)
+public class YamlFooProperties {
+
+    public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<String> getAliases() {
+		return aliases;
+	}
+
+	public void setAliases(List<String> aliases) {
+		this.aliases = aliases;
+	}
+
+	private String name;
+
+    private List<String> aliases;
+
+    // standard getter and setters
+}
